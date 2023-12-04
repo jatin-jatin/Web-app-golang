a14297db003192e0147ac707a32264031fb036c4
jatin lachhwani <jatin.commit@gmail.com>
2023-12-04 17:45:38 +0530

    first line is always package declaration then iport statements then functions two ways to declare variables using var or using := return type written after the function name and not before like in other languages

--- learning-demos/main.go
+++ learning-demos/main.go
@@ -1,7 +1,17 @@
+// practice to name the code file containing main function main.go
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// variables in golang
+	var greeting string
+	greeting = "Goodbye Moonmen"
+	fmt.Println(greeting)
+	words := saysomething()
+	fmt.Println(words)
+}
+
+func saysomething() string {
+	return "something"
 }
