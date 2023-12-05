bf9542077f2ea9c6022395b3ca23688dfe9d0abb
jatin lachhwani <jatinlachhwani666@gmail.com>
2023-12-06 04:19:42 +0530

    using channels chanVar := make(chan int) defer close(chanVar) chanVar <- result consumerVar <- chanVar

--- learning-demos/main.go
+++ learning-demos/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+const numPool = 10
+
+func RandomNumber(n int) int {
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(n)
+	return value
+}
+
+func CalculateValue(intChan chan int) {
+	randomNum := RandomNumber(numPool)
+	intChan <- randomNum
+
+}
+
 func main() {
-	// learn more about packages and modules
+	intChan := make(chan int)
+	defer close(intChan)
+
+	go CalculateValue(intChan)
+	num := <-intChan
+	log.Println(num)
 }
