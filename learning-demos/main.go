a80492d200d7da8d1f90fdddff5c5789e3438898
jatin lachhwani <jatinlachhwani666@gmail.com>
2023-12-06 01:17:34 +0530

    inteface example

--- learning-demos/main.go
+++ learning-demos/main.go
@@ -2,14 +2,50 @@ package main
 
 import "fmt"
 
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	color         string
+	NumberOfTeeth int
+}
+
 func main() {
-	count := 12
-	for i := 0; i < count; i++ {
-		fmt.Println(i)
+	dog := Dog{
+		Name:  "Samson",
+		Breed: "German shephered",
 	}
-	fmt.Println("************************************************")
-	lst := []string{"golang", "python", "java", "cpp"}
-	for i, language := range lst {
-		fmt.Println(i, language)
+	PrintInfo(&dog)
+	gori := Gorilla{
+		Name:          "gauri",
+		color:         "black",
+		NumberOfTeeth: 2,
 	}
+	PrintInfo(&gori)
+}
+
+func (d *Dog) Says() string {
+	return "wooh wooh"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+func (g *Gorilla) Says() string {
+	return "moohhh"
+}
+
+func (g *Gorilla) NumberOfLegs() int {
+	return 2
+}
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
 }
